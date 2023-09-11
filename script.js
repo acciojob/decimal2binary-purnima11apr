4198c93441f9694a03f013868caabfa415afe2d9
purnima11apr <141928600+purnima11apr@users.noreply.github.com>
2023-09-11 17:21:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function decimalToBinary(num) {
   //Write you code here
+	  if (num === 0) {
+        return "0";
+    }
+
+    let binary = "";
+    while (num > 0) {
+        const remainder = num % 2;
+        binary = remainder.toString() + binary;
+        num = Math.floor(num / 2);
+    }
+
+    return binary;
   
 }
 
